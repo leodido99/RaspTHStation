a891eeab54cd8af69fbd390f3d0a3610dd07b456
leodido99 <leonard.bise@gmail.com>
2017-11-05 09:16:51 +0100

    Finished rewriting GPIO module

--- src/Demo/GPIO/gpio.c
+++ src/Demo/GPIO/gpio.c
@@ -23,13 +23,93 @@
 /**
  * Sets the function for the given GPIO pin
  */
-void GPIO_SetFunction(uint32_t pin, _gpio_func func) {
+void GPIO_SetFunction(uint32_t pin, uint8_t func) {
 	uint8_t offset = pin / 10;
 	uint32_t reg = pGPIORegs->GPFSEL[offset];
 	/* 10 GPIO pins per 32 bit registers */
-	uint8_t shift = 3 * (pin % 10));
+	uint8_t shift = 3 * (pin % 10);
 	reg = reg & ~(0x7 << shift);
 	reg = reg | ((func & 0x7) << shift);
 	pGPIORegs->GPFSEL[offset] = reg;
 }
 
+void GPIO_SetDirection(uint32_t pin, _gpio_direction dir) {
+	GPIO_SetFunction(pin, dir);
+}
+
+void GPIO_SetValue(uint32_t pin, bool value) {
+	uint32_t offset=pin/32;
+    uint32_t mask=(1<<(pin%32));
+	if(value) {
+		pGPIORegs->GPSET[offset] |= mask;
+	} else {
+		pGPIORegs->GPCLR[offset] |= mask;
+	}
+}
+
+bool GPIO_GetValue(uint32_t pin) {
+	return ((pGPIORegs->GPLEV[pin/32]) >> (pin%32)) & 1;
+}
+
+void GPIO_SetDetect(uint32_t pin, _gpio_detect detect) {
+	uint32_t mask=(1 << pin);
+	uint32_t offset=pin / 32;
+
+	switch(detect) {
+	case DETECT_RISING:
+		pGPIORegs->GPREN[offset]|=mask;
+		break;
+	case DETECT_FALLING:
+		pGPIORegs->GPFEN[offset]|=mask;
+		break;
+	case DETECT_HIGH:
+		pGPIORegs->GPHEN[offset]|=mask;
+		break;
+	case DETECT_LOW:
+		pGPIORegs->GPLEN[offset]|=mask;
+		break;
+	case DETECT_RISING_ASYNC:
+		pGPIORegs->GPAREN[offset]|=mask;
+		break;
+	case DETECT_FALLING_ASYNC:
+		pGPIORegs->GPAFEN[offset]|=mask;
+		break;
+	case DETECT_NONE:
+		break;
+	}
+}
+
+void GPIO_ResetDetect(uint32_t pin, _gpio_detect detect) {
+	uint32_t mask=~(1 << (pin % 32));
+	uint32_t offset=pin / 32;
+
+	switch(detect) {
+	case DETECT_RISING:
+		pGPIORegs->GPREN[offset]&=mask;
+		break;
+	case DETECT_FALLING:
+		pGPIORegs->GPFEN[offset]&=mask;
+		break;
+	case DETECT_HIGH:
+		pGPIORegs->GPHEN[offset]&=mask;
+		break;
+	case DETECT_LOW:
+		pGPIORegs->GPLEN[offset]&=mask;
+		break;
+	case DETECT_RISING_ASYNC:
+		pGPIORegs->GPAREN[offset]&=mask;
+		break;
+	case DETECT_FALLING_ASYNC:
+		pGPIORegs->GPAFEN[offset]&=mask;
+		break;
+	case DETECT_NONE:
+		break;
+	}
+}
+
+void GPIO_ClearIRQ(uint32_t pin) {
+	uint32_t mask=(1 << (pin % 32));
+	uint32_t offset=pin / 32;
+
+	pGPIORegs->GPEDS[offset]=mask;
+}
--- src/Demo/GPIO/gpio.h
+++ src/Demo/GPIO/gpio.h
@@ -8,14 +8,36 @@
 #ifndef SRC_DEMO_GPIO_GPIO_H_
 #define SRC_DEMO_GPIO_GPIO_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+
 typedef enum {
+	GPIO_INPUT,
+	GPIO_OUTPUT
+} _gpio_direction;
 
+typedef enum {
+	DETECT_NONE,
+	DETECT_RISING,
+	DETECT_FALLING,
+	DETECT_HIGH,
+	DETECT_LOW,
+	DETECT_RISING_ASYNC,
+	DETECT_FALLING_ASYNC
+} _gpio_detect;
 
-} _gpio_func;
+void GPIO_SetFunction(uint32_t pin, uint8_t func);
 
-void GPIO_SetFunction(uint32_t pin, _gpio_func func);
+void GPIO_SetDirection(uint32_t pin, _gpio_direction dir);
 
+void GPIO_SetValue(uint32_t pin, bool value);
 
+bool GPIO_GetValue(uint32_t pin);
 
+void GPIO_SetDetect(uint32_t pin, _gpio_detect detect);
+
+void GPIO_ResetDetect(uint32_t pin, _gpio_detect detect);
+
+void GPIO_ClearIRQ(uint32_t pin);
 
 #endif /* SRC_DEMO_GPIO_GPIO_H_ */
