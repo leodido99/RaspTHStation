3069713571c3ff85ecc341e8b2a98c6ec8c8d7d6
leodido99 <leonard.bise@gmail.com>
2017-12-01 15:26:54 +0100

    Removed old FreeRTOS version

--- src/build/Demo/Drivers/gpio.d
+++
@@ -1,5 +0,0 @@
-/Users/leonard.bise/gitrepo/RaspTHStation/src/build/Demo/Drivers/gpio.o: \
- /Users/leonard.bise/gitrepo/RaspTHStation/src/Demo/Drivers/gpio.c \
- /Users/leonard.bise/gitrepo/RaspTHStation/src/Demo/Drivers/gpio.h
-
-/Users/leonard.bise/gitrepo/RaspTHStation/src/Demo/Drivers/gpio.h:
--- src/build/Demo/Drivers/irq.d
+++
@@ -1,8 +0,0 @@
-/Users/leonard.bise/gitrepo/RaspTHStation/src/build/Demo/Drivers/irq.o: \
- /Users/leonard.bise/gitrepo/RaspTHStation/src/Demo/Drivers/irq.c \
- /Users/leonard.bise/gitrepo/RaspTHStation/src/Demo/Drivers/irq.h \
- /Users/leonard.bise/gitrepo/RaspTHStation/src/Demo/Drivers/bcm2835_intc.h
-
-/Users/leonard.bise/gitrepo/RaspTHStation/src/Demo/Drivers/irq.h:
-
-/Users/leonard.bise/gitrepo/RaspTHStation/src/Demo/Drivers/bcm2835_intc.h:
